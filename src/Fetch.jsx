b49fd41d38c5f15b7339352290dffaabf95ac2a3
kudos2Shef <16985060+kudos2Shef@users.noreply.github.com>
2024-10-04 20:07:51 +0530

    Update Fetch.jsx

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -36,8 +36,8 @@ function Api() {
             </div>
 
         <br/>
-        <img id='line-desk' src='src\assets\images\pattern-divider-desktop.svg'></img>
-        <img id='line-mob' src='src\assets\images\pattern-divider-mobile.svg'></img>
+        <img id='line-desk' src='src/assets/images/pattern-divider-desktop.svg'></img>
+        <img id='line-mob' src='src/assets/images/pattern-divider-mobile.svg'></img>
 
         <br/>
         <button onClick={fetchAdvice} id='dice'>
