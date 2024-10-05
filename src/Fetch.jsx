2a4e284086c3ea2eb96d1e077b0e63574d11c5d3
shef <kashyap.shefali95@gmail.com>
2024-10-05 16:32:17 +0530

    file add changes

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -13,8 +13,6 @@ function Api() {
 			})
 
 			.then((data) => {
-               // console.log(data.slip.advice)
-                //console.log(data.slip.id)
                
                 setAdvice(data.slip.advice)
                 setId(data.slip.id)
@@ -36,16 +34,12 @@ function Api() {
             </div>
 
         <br/>
-        {/*<img id='line-desk' src='src/assets/pattern-divider-desktop.svg'></img>
-        <img id='line-mob' src='src/assets/pattern-divider-mobile.svg'></img>*/
-        }
+        
         <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg" id='line-desk'><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z"/><g transform="translate(212)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
         <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"  id='line-mob'><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z"/><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3"/><rect x="14" width="6" height="16" rx="3"/></g></g></svg>
 
         <br/>
         <button onClick={fetchAdvice} id='dice'>
-          { /* <img src='src/assets/icon-dice.svg' alt='dice' id='icon' ></img>*/
-            }
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" alt='dice' id='icon'><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>	
         </button>
         </div>
